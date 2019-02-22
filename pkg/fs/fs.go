c1c8fd4ad79ac1f09aa8c87e25f06a0306cbd681
Andrew Lavery <laverya@umich.edu>
2019-02-22 14:26:14 -0800

    use an afero fs with the root set at the ship working directory

--- pkg/fs/fs.go
+++ pkg/fs/fs.go
@@ -1,10 +1,17 @@
 package fs
 
 import (
+	"log"
+	"os"
+
 	"github.com/spf13/afero"
 )
 
 // NewBaseFilesystem creates a new Afero OS filesystem
 func NewBaseFilesystem() afero.Afero {
-	return afero.Afero{Fs: afero.NewOsFs()}
+	workingDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return afero.Afero{Fs: afero.NewBasePathFs(afero.NewOsFs(), workingDir)}
 }
