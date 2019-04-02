0e7edbc137873861496e93dc3775b0ac1b716428
Andrew Lavery <laverya@umich.edu>
2019-04-02 11:42:58 -0700

    use upstream url as title for ship app files

--- pkg/lifecycle/daemon/routes_navcycle_getmetadata.go
+++ pkg/lifecycle/daemon/routes_navcycle_getmetadata.go
@@ -2,26 +2,41 @@ package daemon
 
 import (
 	"github.com/gin-gonic/gin"
+
 	"github.com/replicatedhq/ship/pkg/api"
 )
 
 func (d *NavcycleRoutes) getMetadata(release *api.Release) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		switch release.Metadata.Type {
-		case "helm":
+		default:
 			fallthrough
 		case "k8s":
-			c.JSON(200, release.Metadata.ShipAppMetadata)
+			fallthrough
+		case "helm":
+			info := release.Metadata.ShipAppMetadata
+			c.JSON(200, info)
 			return
 		case "runbook.replicated.app":
 			fallthrough
 		case "replicated.app":
-			fallthrough
-		case "inline.replicated.app":
 			c.JSON(200, map[string]interface{}{
 				"name": release.Metadata.ChannelName,
 				"icon": release.Metadata.ChannelIcon,
 			})
+			return
+		case "inline.replicated.app":
+			info := map[string]interface{}{
+				"name": release.Metadata.ChannelName,
+				"icon": release.Metadata.ChannelIcon,
+			}
+
+			// the raw url tends to be ugly, but better than nothing
+			if info["name"] == "" {
+				info["name"] = release.Metadata.ShipAppMetadata.URL
+			}
+
+			c.JSON(200, info)
 			return
 		}
 	}
